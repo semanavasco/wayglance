0c623b723e087142e44e2f2fb785bcc0706953c2
semanavasco <semanavasco@gmail.com>
2025-06-18 22:10:45 +0200

    Improved project + Bug fixes
    Project improvements :
    - Using system locales;
    - Using cairo to draw our overlays on top of a transparent background;
    - Using stronger styling rules to overtake global theme (bug with
    transparent background not being transparent);
    - Added comprehensive comments.
    
    Bug fixes :
    - Using stronger styling rules to overtake global theme.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+#include <cairomm/context.h>
 #include <chrono>
+#include <clocale>
 #include <format>
 #include <gtkmm.h>
+
 extern "C" {
 #include <gtk4-layer-shell.h>
 }
@@ -12,60 +15,60 @@ public:
   virtual ~WallpaperWindow();
 
 protected:
-  Gtk::Box m_main_box;
+  Gtk::Overlay m_overlay;
+  Gtk::DrawingArea m_drawing_area;
+  Gtk::Box m_label_box;
+
   Gtk::Label m_time_label;
   Gtk::Label m_date_label;
 
+  // Methods
   bool update_labels();
+  void on_draw(const Cairo::RefPtr<Cairo::Context> &cr, int width, int height);
 };
 
 // Window's constructor
 WallpaperWindow::WallpaperWindow()
-    : m_main_box(Gtk::Orientation::VERTICAL, 10) {
-  // Layer shell configuration
+    : m_label_box(Gtk::Orientation::VERTICAL, 10) {
+  // Window configuration
+  set_title("Hyprland Widget");
+  set_child(m_overlay);
+
+  // DrawingArea configuration
+  // Defining the function that'll be calle dfor redraws
+  m_drawing_area.set_draw_func(sigc::mem_fun(*this, &WallpaperWindow::on_draw));
+
+  // Overlay configuration
+  m_overlay.set_child(m_drawing_area);
+  m_overlay.add_overlay(m_label_box);
+
+  // Configuring label box
+  m_label_box.set_valign(Gtk::Align::CENTER);
+  m_label_box.set_halign(Gtk::Align::CENTER);
+  m_label_box.append(m_time_label);
+  m_label_box.append(m_date_label);
+
+  // Configuring layer shell
   gtk_layer_init_for_window((GtkWindow *)gobj());
-
-  // Setting the layer as background
   gtk_layer_set_layer((GtkWindow *)gobj(), GTK_LAYER_SHELL_LAYER_BACKGROUND);
-
-  // Anchoring the window to the 4 corners of the screen
   gtk_layer_set_anchor((GtkWindow *)gobj(), GTK_LAYER_SHELL_EDGE_LEFT, true);
   gtk_layer_set_anchor((GtkWindow *)gobj(), GTK_LAYER_SHELL_EDGE_RIGHT, true);
   gtk_layer_set_anchor((GtkWindow *)gobj(), GTK_LAYER_SHELL_EDGE_TOP, true);
   gtk_layer_set_anchor((GtkWindow *)gobj(), GTK_LAYER_SHELL_EDGE_BOTTOM, true);
-
-  // Deactivate keyboard interactivity
   gtk_layer_set_keyboard_mode((GtkWindow *)gobj(),
                               GTK_LAYER_SHELL_KEYBOARD_MODE_NONE);
 
-  // Initial window configuration
-  set_title("Hyprland Widget");
-  set_child(m_main_box); // Adding the main box to the window
-
-  // Configuring the main window to center its content
-  m_main_box.set_valign(Gtk::Align::CENTER);
-  m_main_box.set_halign(Gtk::Align::CENTER);
-
-  // Adding labels to the main box
-  m_main_box.append(m_time_label);
-  m_main_box.append(m_date_label);
-
-  // Adding css classes to the labels
+  // Configuring CSS provider and styles
+  set_name("wallpaper_window");
   m_time_label.add_css_class("time");
   m_date_label.add_css_class("date");
-
-  // Creating a CSS loader
   auto css_provider = Gtk::CssProvider::create();
-  // Loading the file from a specified path
   css_provider->load_from_path("style.css");
-  // Applying the css provider to the whole display
   Gtk::StyleProvider::add_provider_for_display(
-      get_display(), css_provider, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+      get_display(), css_provider, GTK_STYLE_PROVIDER_PRIORITY_USER);
 
-  // Configuring the timer
+  // Configuring timer
   update_labels();
-
-  // Connecting update_labels to a signal that triggers every 1000ms
   Glib::signal_timeout().connect(
       sigc::mem_fun(*this, &WallpaperWindow::update_labels), 1000);
 }
@@ -73,23 +76,28 @@ WallpaperWindow::WallpaperWindow()
 // Destructor
 WallpaperWindow::~WallpaperWindow() {}
 
-// Label update method
+// Methods
 bool WallpaperWindow::update_labels() {
-  // Getting the current timestamp
   const auto now = std::chrono::system_clock::now();
-
-  // Formatting and updating the hour label
   m_time_label.set_text(std::format("{:%H:%M}", now));
-
-  // Formatting and updating the date label
   m_date_label.set_text(std::format("{:%A %d %B %Y}", now));
 
-  // Returns 'true' so that the timer continues running
   return true;
+}
+
+void WallpaperWindow::on_draw(const Cairo::RefPtr<Cairo::Context> &cr,
+                              int width, int height) {
+  // Setting the drawing's color to transparent black
+  cr->set_source_rgba(0.0, 0.0, 0.0, 0.0);
+  // Painting the wole surface with this transparent color
+  cr->paint();
 }
 
 // Main entry point
 int main(int argc, char *argv[]) {
+  // Setting program's locale to the environment one
+  std::setlocale(LC_ALL, "");
+
   // Creating an instance of the GTK app
   auto app = Gtk::Application::create("org.example.hyprwidget");
 
