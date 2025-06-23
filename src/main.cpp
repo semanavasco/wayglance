aaf4d2b9d3497caa9ae8862e609810c6ba85df38
semanavasco <semanavasco@gmail.com>
2025-06-23 15:47:23 +0200

    Created an AppManager class
    The AppManager class is responsible of the app's lifecycle
    It handles new monitor inputs, removed monitors and the app itself

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,21 @@
-#include "Wayglance.hpp"
+#include "managers/AppManager.hpp"
 #include "managers/ConfigManager.hpp"
 #include <cairomm/context.h>
 #include <gdkmm/display.h>
 #include <gdkmm/monitor.h>
 #include <gtkmm.h>
-#include <iostream>
 
 extern "C" {
 #include <gtk4-layer-shell.h>
 }
 
-// Main entry point
 int main(int argc, char *argv[]) {
   auto app = Gtk::Application::create("io.github.semanavasco.wayglance");
+
   auto config_manager = std::make_shared<ConfigManager>();
+  AppManager app_manager(app, config_manager);
 
-  // Connecting to the activate signal on startup
-  app->signal_activate().connect([&]() {
-    // Getting the default display
-    auto display = Gdk::Display::get_default();
-    if (!display)
-      return;
+  app->signal_activate().connect([&]() { app_manager.run(); });
 
-    // Getting a list of all monitors
-    auto monitors = display->get_monitors();
-    if (!monitors)
-      return;
-
-    // Getting the number of monitors via the C function
-    guint n_monitors = g_list_model_get_n_items(monitors->gobj());
-    std::cout << "Detected " << n_monitors << " monitor(s)" << std::endl;
-
-    for (guint i = 0; i < n_monitors; i++) {
-      // Getting the raw C object
-      void *monitor_gobject = g_list_model_get_item(monitors->gobj(), i);
-      if (!monitor_gobject)
-        continue;
-
-      // Wrapping the C-Object in a C++ Glib::RefPtr to manipulate it
-      auto monitor_wrapper = Glib::wrap((GObject *)monitor_gobject);
-
-      // Ensuring it's a monitor
-      auto monitor = std::dynamic_pointer_cast<Gdk::Monitor>(monitor_wrapper);
-      if (!monitor)
-        continue;
-
-      std::cout << "Creating widget for monitor n°" << i << std::endl;
-
-      // Creating a new instance
-      auto window =
-          new Wayglance(config_manager, (GdkMonitor *)monitor->gobj());
-      app->add_window(*window);
-      window->show();
-    }
-  });
-
-  // Displaying the WallpaperWindow
   return app->run(argc, argv);
 }
---
+++ src/managers/AppManager.cpp
@@ -0,0 +1,108 @@
+#include "AppManager.hpp"
+#include "ConfigManager.hpp"
+#include <iostream>
+#include <unordered_set>
+
+// Constructor
+AppManager::AppManager(Glib::RefPtr<Gtk::Application> app,
+                       std::shared_ptr<ConfigManager> config_manager)
+    : m_app(app), m_config_manager(config_manager) {}
+
+// Destructor
+AppManager::~AppManager() {}
+
+// Methods
+void AppManager::run() {
+  // Getting the default display manager
+  auto display = Gdk::Display::get_default();
+  if (!display) {
+    std::cerr << "Error: Could not get a GDK display" << std::endl;
+    return;
+  }
+
+  // Connecting to monitors signal
+  auto monitors = display->get_monitors();
+  if (monitors) {
+    monitors->signal_items_changed().connect(
+        [this](guint position, guint removed, guint added) {
+          std::cout << "Monitors configuration has changed, updating windows"
+                    << added << std::endl;
+          update_monitors();
+        });
+  }
+
+  // Initial monitors configuration
+  update_monitors();
+}
+
+void AppManager::update_monitors() {
+  // Getting the current monitors list
+  auto display = Gdk::Display::get_default();
+  if (!display)
+    return;
+  auto current_monitors_list = display->get_monitors();
+  if (!current_monitors_list)
+    return;
+
+  // Creating a set of current monitors for efficient search
+  std::unordered_set<GdkMonitor *> current_monitor_set;
+  for (guint i = 0; i < current_monitors_list->get_n_items(); ++i) {
+    auto monitor_obj = current_monitors_list->get_object(i);
+    current_monitor_set.insert(
+        reinterpret_cast<GdkMonitor *>(monitor_obj->gobj()));
+  }
+
+  // Removing monitors that are no longer available
+  std::vector<Glib::RefPtr<Gdk::Monitor>> monitors_to_remove;
+  for (const auto &pair : m_windows)
+    if (current_monitor_set.find(pair.first) == current_monitor_set.end())
+      monitors_to_remove.push_back(Glib::wrap(pair.first));
+
+  for (const auto &monitor : monitors_to_remove)
+    remove_monitor(monitor);
+
+  // Adding missing windows
+  for (guint i = 0; i < current_monitors_list->get_n_items(); ++i) {
+    auto monitor_object = current_monitors_list->get_object(i);
+    auto monitor = std::dynamic_pointer_cast<Gdk::Monitor>(monitor_object);
+    if (monitor)
+      add_monitor(monitor);
+  }
+}
+
+void AppManager::add_monitor(const Glib::RefPtr<Gdk::Monitor> &monitor) {
+  if (m_windows.count(monitor->gobj())) {
+    std::cout << "Warning: A window for this monitor already exists, ignoring"
+              << std::endl;
+    return;
+  }
+
+  std::cout << "Creating a Wayglance window for a monitor" << std::endl;
+
+  // Creating a window instance
+  auto window = new Wayglance(m_config_manager, monitor->gobj());
+  m_app->add_window(*window);
+  window->show();
+
+  // Store the window
+  m_windows[monitor->gobj()] = window;
+}
+
+void AppManager::remove_monitor(const Glib::RefPtr<Gdk::Monitor> &monitor) {
+  std::cout << "Monitor removed signal received" << std::endl;
+
+  // Find the window associated with the removed monitor
+  auto it = m_windows.find(monitor->gobj());
+
+  if (it != m_windows.end()) {
+    Wayglance *window_to_remove = it->second;
+    std::cout << "Closing Wayglance window for the removed monitor"
+              << std::endl;
+
+    // Close the window (GTK manages its destruction)
+    window_to_remove->close();
+
+    // Remove the window from our managed windows
+    m_windows.erase(it);
+  }
+}
