9b5548d19543a355bbfa8a299006b15e374bab0c
semanavasco <semanavasco@users.noreply.github.com>
2026-03-15 18:12:25 +0000

    deploy: 1271e6b93f792f2878e3b9961844fca2309a5ea4

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8b0f6fe1.js',
+                path_to_root + 'searchindex-52016b87.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
